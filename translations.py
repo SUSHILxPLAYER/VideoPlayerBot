755cb8c663006a7bc3ff4ef7f799b785a64317da
SUSHILxPLAYER <100741130+SUSHILxPLAYER@users.noreply.github.com>
2022-03-12 15:26:24 +0530

    Update translations.py

--- translations.py
+++ translations.py
@@ -2,7 +2,7 @@ from config import ASSISTANT_NAME
 from helpers.bot_utils import BOT_NAME, USERNAME
 
 
-START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @ImSafone!** 👑"
+START_TEXT = f"👋🏻 **Hello**, \n\nThis is **{BOT_NAME}** \nI Can Stream Lives, Radios, YouTube Videos & Telegram Audio / Video Files On Voice Chat Of Telegram Groups. Let's Enjoy Cinematic View Of Group Video Player With Your Friends 😉! \n\n**Made With ❤️ By @NAVYA_OP!** 👑"
 HELP_TEXT = f"""
 🛠-- **Setting Up Bot**:--
 
@@ -13,10 +13,10 @@ HELP_TEXT = f"""
 ⚔️-- **Available Commands**:--
 
 \u2022 `/play` - Stream An Audio
-\u2022 `/stream` - Stream An Video
-\u2022 `/pause` - Pause Current Stream
-\u2022 `/resume` - Resume Paused Stream
-\u2022 `/endstream` - End Stream & Left VC
-\u2022 `/restart` - Restart Bot (Sudo Only)
+\u2022 `/s` - Stream An Video
+\u2022 `/pa` - Pause Current Stream
+\u2022 `/re` - Resume Paused Stream
+\u2022 `/ends` - End Stream & Left VC
+\u2022 `/rs` - Restart Bot (Sudo Only)
 """
 ABOUT_TEXT = f"💡-- **Information**:-- \n\nThis bot is created for streaming videos in telegram group video chats using several methods from WebRTC. Powered by pytgcalls the async client API for the Telegram Group Calls and Pyrogram the telegram MTProto API Client Library and Framework in Pure Python for Users and Bots. \n\n**This bot licensed under GNU-GPL 3.0 License!**"
